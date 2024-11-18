af4674686b9ab0d8fd06fd962fa1e4c368a3f2a6
Egor1o <egorklo147@gmail.com>
2024-11-18 19:43:40 +0200

    Content for the page. Some changes to the modal and corresponding components.

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -4,12 +4,13 @@ import '../styles/PersonCard.css';
 type Props = {
   handleClick: () => void
   label: string
+  styles? : string
 };
 
 const Button: React.FC<Props> = props => {
 
   return (
-    <div className="bg-[#EC4D4D] p-2 min-w-24 rounded-md hover:shadow-2xl hover:shadow-cyan-500/50">
+    <div className={`bg-[#EC4D4D] min-w-24 rounded-md ${props.styles ? props.styles : "p-2"}`}>
       <button onClick={props.handleClick} className="text-white hover:text-blue-900">
         {props.label}
       </button>
