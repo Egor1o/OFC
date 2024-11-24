5063d3c3802472c2a7e2290a961410f1b3fff1e1
Egor1o <egorklo147@gmail.com>
2024-11-24 23:23:44 +0200

    MDX-parser.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import remarkToc from 'remark-toc';
 export default defineConfig({
   server: {
     port: 3000,
     host: true
   },
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  markdown: {
+    remarkPlugins: [ [remarkToc, { heading: 'toc', maxDepth: 3 } ] ],
+  },
 });
---
+++ frontend/src/common/common.ts
@@ -0,0 +1,22 @@
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkGfm from 'remark-gfm';
+import remarkRehype from 'remark-rehype';
+import rehypeAddClasses from 'rehype-add-classes';
+import rehypeStringify from 'rehype-stringify';
+
+const getHTMLfromMDX = async (mdx: string, styles: Object) => {
+  return await unified()
+    .use(remarkParse)
+    .use(remarkFrontmatter)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeAddClasses, styles)
+    .use(rehypeStringify)
+    .process(mdx)
+}
+
+export {
+  getHTMLfromMDX
+}
